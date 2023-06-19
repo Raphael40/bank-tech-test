3817a805ddb1fe07cb1bff3a57bfd3d993d1bb0a
Julian Festing <julfesting@gmail.com>
2023-06-19 16:55:24 +0100

    passed test for including 1000 in statement if 1000 is deposited

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,18 @@
 class Account
   def initialize
     @balance = 0
+    @transactions = []
   end
   
   def print_statement
     puts @balance
-    return 'date || credit || debit || balance'
+  if @balance > 0
+    return "date || credit || debit || balance' \n || || || 1000"
+  end
+    return "date || credit || debit || balance"
   end
 
+  def deposit(sum)
+    @balance += sum
+  end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -8,12 +8,11 @@ RSpec.describe 'Account' do
       expect(account.print_statement).to eq 'date || credit || debit || balance'
     end
 
-    xit 'displays a transaction of 1000 after depositing 1000' do
+    it 'displays a transaction of 1000 after depositing 1000' do
       account = Account.new
       account.deposit(1000)
 
-      expect(account.print_balance).to eq 1000
+      expect(account.print_statement).to include  "1000"
     end
-
   end
 end
