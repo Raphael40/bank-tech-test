648c7bdec0e9eb2467791ea58a7de839b6c61f84
Julian Festing <julfesting@gmail.com>
2023-06-19 17:23:29 +0100

    a single transaction of 1000 shows twice on the statement, for credit and balance

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -8,11 +8,19 @@ RSpec.describe 'Account' do
       expect(account.print_statement).to eq 'date || credit || debit || balance'
     end
 
-    it 'displays a transaction of 1000 after depositing 1000' do
+    it 'includes 1000 in statement after depositing 1000' do
       account = Account.new
       account.deposit(1000)
 
       expect(account.print_statement).to include  "1000"
+    end
+
+    it 'includes 1000 in twice after depositing 1000' do
+      account = Account.new
+      account.deposit(1000)
+      result = account.print_statement.gsub(/[^1000]/, '')
+
+      expect(result).to eq "10001000"
     end
   end
 end
