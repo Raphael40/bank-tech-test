85867c6eb0684278c0981f13ff9991e3efedd1d3
Julian Festing <julfesting@gmail.com>
2023-06-23 13:50:10 +0100

    passsed account unit test for depositing then withdrawing

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe 'Account' do
     expect(transaction[0][1]).to eq '10.00'
     expect(transaction[0][3]).to eq '10.00'
   end
+
+  it 'deposits then withdraws a sum' do
+    account = Account.new
+    account.deposit(10)
+    account.withdraw(5)
+    transaction = account.instance_variable_get(:@transactions)
+
+    expect(transaction[0][1]).to eq '10.00'
+    expect(transaction[0][3]).to eq '10.00'
+    expect(transaction[1][2]).to eq '5.00'
+    expect(transaction[1][3]).to eq '5.00'
+  end
 end
