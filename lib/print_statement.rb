28fe1f8919b1cc3a5b2013c0a2ab71e0a3a53ef0
Julian Festing <julfesting@gmail.com>
2023-07-04 17:16:16 +0100

    changed name of transactions parameter

--- lib/print_statement.rb
+++ lib/print_statement.rb
@@ -1,7 +1,7 @@
 class Print_statement 
 
-  def print_statement(transaction)
-    transaction_history = transaction.instance_variable_get(:@transactions)
+  def print_statement(transactions)
+    transaction_history = transactions.instance_variable_get(:@transactions)
     if transaction_history.length.positive?
       "date || credit || debit || balance \n#{transaction_history.map { |transaction| transaction.join(' || ') }.join(" \n")}"
     else
